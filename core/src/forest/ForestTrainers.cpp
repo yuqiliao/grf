91be504bc3c7bf3cadfdedccac5ec487a8ab042b
Rina Siller Friedberg <rinafriedberg@gmail.com>
2017-04-17 20:50:23 -0700

    added forest trainer for locally linear forests

--- core/src/forest/ForestTrainers.cpp
+++ core/src/forest/ForestTrainers.cpp
@@ -65,3 +65,18 @@ ForestTrainer ForestTrainers::regression_trainer(Data* data,
 
   return ForestTrainer(observables, relabeling_strategy, splitting_rule_factory, prediction_strategy);
 }
+
+ForestTrainer ForestTrainers::locally_linear_trainer(Data* data,
+                                                   size_t outcome_index,
+                                                   size_t covariates_index,
+                                                   double lambda) {
+    std::unordered_map<size_t, size_t> observables = {
+        {Observations::OUTCOME, outcome_index},
+        {Observations::COVARIATES, covariates_index}};
+    
+    std::shared_ptr<RelabelingStrategy> relabeling_strategy(new LocallyLinearRelabelingStrategy(lambda));
+    std::shared_ptr<SplittingRuleFactory> splitting_rule_factory(new RegressionSplittingRuleFactory(data));
+    std::shared_ptr<PredictionStrategy> prediction_strategy(new LocallyLinearPredictionStrategy(lambda));
+    
+    return ForestTrainer(observables, relabeling_strategy, splitting_rule_factory, prediction_strategy);
+}
