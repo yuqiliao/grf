6264e8bbfe139721ff1c9a159dd62276d983c215
Rina Siller Friedberg <rinafriedberg@gmail.com>
2017-08-01 17:21:09 -0700

    testing code; added simulations for clarity

--- r-package/image-test.R
+++ r-package/image-test.R
@@ -6,22 +6,21 @@ n = 600
 frac = 0.70
 mtry = 4
 sigma = 0.1
-names = c("yay", "wooo")
-mu = function(xx) log(1 + exp(6 * xx))
-leaf.size = 1
+leaf.size = 2
 lambda = 0.5
 
+mu = function(xx) log(1 + exp(6 * xx))
 X = matrix(runif(p*n, -1, 1), n, p)
 Y = sapply(X[,1], mu) + sigma * rnorm(n)
-llf = locally.linear.forest(X, Y, sample.fraction = frac, mtry = mtry, variable_names = names, lambda = lambda, 
+llf = locally.linear.forest(X, Y, sample.fraction = frac, mtry = mtry, lambda = lambda, 
                             num.trees = 500, min.node.size = leaf.size, ci_group_size = 1)
 yhat = predict(llf)
 error = sum((yhat-Y)**2)/n
 xx = seq(-1,1,length=200)
 
-setwd("~/Desktop")
-png(filename="boundary.png")
+#setwd("~/Desktop")
+#png(filename="boundary.png")
 plot(X[,1],yhat, main="Locally Linear Forest",ylim=c(0,6),xlab="x",ylab="y")
 lines(xx,mu(xx),lwd=2,col=2)
 text(x=-0.5, y=1, labels=paste("error=",as.character(round(error,4))))
-dev.off()
+#dev.off()
--- r-package/simulation-test.R
+++ r-package/simulation-test.R
@@ -1,45 +1,38 @@
 rm(list = ls())
 library(gradient.forest)
 
-p = 10
-n = 600
-frac = 0.70
-mtry = 4
-sigma = 0.1
-names = c("yay", "wooo")
-mu = function(xx) log(1 + exp(6 * xx))
-leaf.size = 5
-#lambda = 1 
-
-# prediction error test
-grid = sapply(c(0,3,5,7,10,15), function(lambda){
-  prederrors = sapply(1:5, function(i){
+errors = function(n = 600, p = 10, frac = 0.70, mtry = ceiling(p/3), sigma = 0.1, leaf.size = 1, lambda = 1, numreps = 10, cutoff = 0.8){
+  mses = rep(-1, numreps)
+  tail.mses = rep(-1, numreps)
+  for(i in 1:numreps){
     X = matrix(runif(p*n, -1, 1), n, p)
     Y = sapply(X[,1], mu) + sigma * rnorm(n)
     
-    llf = locally.linear.forest(X, Y, sample.fraction = frac, mtry = mtry, variable_names = names, 
-                                lambda = lambda, num.trees = 500, min.node.size = leaf.size, ci_group_size = 1)
+    llf = locally.linear.forest(X, Y, sample.fraction = frac, mtry = mtry,lambda = lambda, 
+                                num.trees = 500, min.node.size = leaf.size, ci_group_size = 1)
     
     newX = matrix(runif(p*n, -1, 1), n, p)
     newY = sapply(newX[,1], mu) + sigma * rnorm(n)
     
     newtemp = predict(llf, newdata=newX)
-    sum((newtemp-newY)**2)/n
-  })
-  
-  print(paste("average prediction error for lambda = ", lambda, ", leaf size = ", leaf.size, ", p = ", p, ", and sigma = ", sigma))
-  print(mean(prederrors))
-})
+    mses[i] = sum((newtemp-newY)**2)/n
+    
+    temp = 0
+    temp.len = 0
+    for(j in 1:n){
+      if(newX[j,1] > cutoff){
+        temp = temp + (newtemp[j] - newY[j])**2 
+        temp.len = temp.len + 1
+      }
+    }
+    temp = temp/temp.len
+    tail.mses[i] = temp
+  }
+  return(c(mean(mses), mean(tail.mses)))
+}
 
-#prederrors = sapply(1:5, function(i){
-#  X = matrix(runif(p*n, -1, 1), n, p)
-#  Y = sapply(X[,1], mu) + sigma * rnorm(n)
-#  llf = locally.linear.forest(X, Y, sample.fraction = frac, mtry = mtry, variable_names = names, 
-#                              lambda = lambda, num.trees = 500, min.node.size = leaf.size, ci_group_size = 1)
-#  newX = matrix(runif(p*n, -1, 1), n, p)
-#  newY = sapply(newX[,1], mu) + sigma * rnorm(n)
-#  newtemp = predict(llf, newdata=newX)
-##  sum((newtemp-newY)**2)/n
-#})
-#print(paste("average prediction error for lambda = ", lambda, ", leaf size = ", leaf.size, ", p = ", p, ", and sigma = ", sigma))
-#print(mean(prederrors))
+lambdas = seq(0,5,length=20)
+test.lambda = sapply(lambdas, function(lambda){
+  errors(lambda=lambda, numreps=20)[1]
+})
+plot(lambdas, test.lambda, 'l', xlab = "lambda", ylab="MSE", main = "MSE by Regularization")
