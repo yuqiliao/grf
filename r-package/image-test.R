52bed6253a43b7a877d5401d385a16c1bd472901
Rina Siller Friedberg <rinafriedberg@gmail.com>
2017-08-02 17:55:05 -0700

    added updated tests

--- r-package/image-test.R
+++ r-package/image-test.R
@@ -7,20 +7,25 @@ frac = 0.70
 mtry = 4
 sigma = 0.1
 leaf.size = 2
-lambda = 0.5
+lambda = 2.5
 
 mu = function(xx) log(1 + exp(6 * xx))
 X = matrix(runif(p*n, -1, 1), n, p)
 Y = sapply(X[,1], mu) + sigma * rnorm(n)
-llf = locally.linear.forest(X, Y, sample.fraction = frac, mtry = mtry, lambda = lambda, 
+
+X = scale(X)
+
+llf = locally.linear.forest(X, Y - mean(Y), sample.fraction = frac, mtry = mtry, lambda = lambda, 
                             num.trees = 500, min.node.size = leaf.size, ci_group_size = 1)
-yhat = predict(llf)
+yhat = predict(llf) + mean(Y)
 error = sum((yhat-Y)**2)/n
 xx = seq(-1,1,length=200)
 
+X = X * attr(X, 'scaled:scale') + attr(X, 'scaled:center')
+
 #setwd("~/Desktop")
 #png(filename="boundary.png")
-plot(X[,1],yhat, main="Locally Linear Forest",ylim=c(0,6),xlab="x",ylab="y")
+plot(X[,1],yhat, main="",ylim=c(0,6),xlab="x",ylab="y")
 lines(xx,mu(xx),lwd=2,col=2)
-text(x=-0.5, y=1, labels=paste("error=",as.character(round(error,4))))
+text(x=-0.8, y=1, labels=paste("error=",as.character(round(error,4))), cex=0.6)
 #dev.off()
--- r-package/simulation-test.R
+++ r-package/simulation-test.R
@@ -1,20 +1,32 @@
 rm(list = ls())
 library(gradient.forest)
 
-errors = function(n = 600, p = 10, frac = 0.70, mtry = ceiling(p/3), sigma = 0.1, leaf.size = 1, lambda = 1, numreps = 10, cutoff = 0.8){
+mu = function(xx) log(1 + exp(6 * xx))
+
+errors = function(p, n = 600, frac = 0.70, mtry = ceiling(p/3), sigma = 0.1, leaf.size = 1, lambda = 1, 
+                  numreps = 10, cutoff = 0.8, standardize = TRUE){
   mses = rep(-1, numreps)
   tail.mses = rep(-1, numreps)
   for(i in 1:numreps){
     X = matrix(runif(p*n, -1, 1), n, p)
     Y = sapply(X[,1], mu) + sigma * rnorm(n)
     
-    llf = locally.linear.forest(X, Y, sample.fraction = frac, mtry = mtry,lambda = lambda, 
+    if(standardize){
+      X = scale(X)
+    }
+    
+    llf = locally.linear.forest(X, Y - mean(Y), sample.fraction = frac, mtry = mtry,lambda = lambda, 
                                 num.trees = 500, min.node.size = leaf.size, ci_group_size = 1)
     
     newX = matrix(runif(p*n, -1, 1), n, p)
     newY = sapply(newX[,1], mu) + sigma * rnorm(n)
     
-    newtemp = predict(llf, newdata=newX)
+    if(standardize){
+      newtemp = predict(llf, newdata=scale(newX)) + mean(Y)
+    }else{
+      newtemp = predict(llf, newdata = newX) + mean(Y)
+    }
+    
     mses[i] = sum((newtemp-newY)**2)/n
     
     temp = 0
@@ -31,8 +43,23 @@ errors = function(n = 600, p = 10, frac = 0.70, mtry = ceiling(p/3), sigma = 0.1
   return(c(mean(mses), mean(tail.mses)))
 }
 
-lambdas = seq(0,5,length=20)
+## Examine Effect of Regularization ##
+
+lambdas = seq(0,10,length=41)
 test.lambda = sapply(lambdas, function(lambda){
-  errors(lambda=lambda, numreps=20)[1]
+  errors(p=10,lambda=lambda)[1]
 })
-plot(lambdas, test.lambda, 'l', xlab = "lambda", ylab="MSE", main = "MSE by Regularization")
+plot(lambdas, test.lambda, 'l', xlab = "lambda", ylab="MSE", main = "MSE by Regularization (unscaled)")
+
+#################################################
+## Compare Effects of Standardizing and Lambda ##
+#################################################
+
+lambdas = seq(0,10,length=41)
+
+reg.lambda = sapply(lambdas, function(lambda){errors(p=10,lambda=lambda, standardize = TRUE)[1]})
+unreg.lambda = sapply(lambdas, function(lambda){errors(p=10,lambda=lambda, standardize = FALSE)[1]})
+
+plot(lambdas, reg.lambda, 'l', lwd = 2, ylim = range(reg.lambda, unreg.lambda), xlab = "lambda", ylab="MSE", main = "MSE by Regularization")
+lines(lambdas, unreg.lambda, 'l', col="blue", lwd=2)
+legend("topleft", c("Standardized", "Unstandardized"), col=c("black","blue"), cex=0.5, lwd=c(2,2))
